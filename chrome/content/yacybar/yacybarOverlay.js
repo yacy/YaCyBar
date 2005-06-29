59030eb1ddc721d097bcd9df25ac88e7d77ae00f
allo <allo@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2005-06-29 15:06:19 +0000

    The files of the toolbar

---
+++ chrome/content/yacybar/yacybarOverlay.js
@@ -0,0 +1,12 @@
+var gBrowser = document.getElementById("content");
+function search(){
+	var searchwords=document.getElementById("search").value;
+	alert(searchwords);
+	loadOneOrMoreURIs("http://localhost:8080/index.html?search="+searchwords);
+}
+function blacklistpage(){
+	var url=window.content.prompt("Blacklist URL:", window._content.location);
+	if(url != null && url != ""){
+		gBrowser.addTab("http://localhost:8080/Blacklist_p.html?addbutton=addbutton&filename=default.black&newItem="+url);
+	}
+}
---
+++ makerelease.sh
@@ -0,0 +1,6 @@
+cd chrome
+rm -f yacybar.jar
+zip -r yacybar.jar *
+cd ..
+rm yacybar.xpi
+zip -r yacybar.xpi *

203a4313e020d0675572cb149b274713d42ba22a
theli <theli@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2006-01-24 16:13:59 +0000

    *) adding possibility to change crawling depth

--- chrome/content/yacybarOverlay.js
+++ chrome/content/yacybarOverlay.js
@@ -1,5 +1,6 @@
 var baseURL  = "http://localhost:8080";
 var gBrowser = document.getElementById("content");
+var crawlingDepth = 0;
 
 function search(){
 	var searchwords=document.getElementById("search").value;
@@ -14,10 +15,14 @@ function blacklistpage(){
 	}
 }	
 
+function changeCrawlingDepth(newCrawlingDepth) {
+	crawlingDepth = newCrawlingDepth;
+}
+
 function crawlpage() {
-    var url = window.content.prompt("Index URL:", window._content.location);
+    var url = window.content.prompt("Index URL with depth '" + crawlingDepth + "':", window._content.location);
 	if(url != null && url != ""){
-		window.open(baseURL + '/QuickCrawlLink_p.html?localIndexing=on&crawlingQ=on&xdstopw=on&title='+escape(document.title)+'&url='+url,'_blank','height=150,width=500,resizable=yes,scrollbar=no,directory=no,menubar=no,location=no');
+		window.open(baseURL + '/QuickCrawlLink_p.html?localIndexing=on&crawlingQ=on&crawlingDepth=' + crawlingDepth + '&xdstopw=on&title='+escape(document.title)+'&url='+url,'_blank','height=150,width=500,resizable=yes,scrollbar=no,directory=no,menubar=no,location=no');
 	}
 }
 	

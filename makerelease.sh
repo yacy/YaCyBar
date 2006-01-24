473a0780461c6ddfcfdfd6e246fcc172716b6466
allo <allo@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2006-01-24 20:37:42 +0000

    modiefied for onlineediting (with disabled XUL-Cache)

--- makerelease.sh
+++ makerelease.sh
@@ -1,6 +1,6 @@
-cd chrome
-rm -f yacybar.jar
-zip -r yacybar.jar * -x *.svn/*
-cd ..
+#cd chrome
+#rm -f yacybar.jar
+#zip -r yacybar.jar * -x *.svn/*
+#cd ..
 rm yacybar.xpi
 zip -r yacybar.xpi * -x *.svn/*

d58b746b51e8c84e25fe985ce86ed8f1622ad303
theli <theli@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2006-01-24 12:21:22 +0000

    *) reenabling yacybar for firefox 1.5 *) adding ant buildfile *) moving files from content/yacybar into content/

--- chrome/content/yacybarOverlay.js
+++ chrome/content/yacybarOverlay.js
@@ -2,7 +2,7 @@ var gBrowser = document.getElementById("content");
 function search(){
 	var searchwords=document.getElementById("search").value;
 	alert(searchwords);
-	loadOneOrMoreURIs("http://localhost:8080/index.html?search="+searchwords);
+	loadOneOrMoreURIs("http://brain:18090/index.html?search="+searchwords);
 }
 function blacklistpage(){
 	var url=window.content.prompt("Blacklist URL:", window._content.location);

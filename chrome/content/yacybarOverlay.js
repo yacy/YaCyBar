b14c02c8b2abc55139865d61408011c70f7b4d41
theli <theli@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2006-01-24 14:55:20 +0000

    *) yacy bar version with button to index current opened page

--- chrome/content/yacybarOverlay.js
+++ chrome/content/yacybarOverlay.js
@@ -1,19 +1,24 @@
+var baseURL  = "http://localhost:8080";
 var gBrowser = document.getElementById("content");
 
 function search(){
 	var searchwords=document.getElementById("search").value;
-	alert(searchwords);
-	loadOneOrMoreURIs("http://localhost:8080/index.html?search="+searchwords);
+	// alert(searchwords);
+	loadOneOrMoreURIs(baseURL + "/index.html?search="+searchwords);
 }
 
 function blacklistpage(){
 	var url=window.content.prompt("Blacklist URL:", window._content.location);
 	if(url != null && url != ""){
-		gBrowser.addTab("http://localhost:8080/Blacklist_p.html?addbutton=addbutton&filename=default.black&newItem="+url);
+		gBrowser.addTab(baseURL + "/Blacklist_p.html?addbutton=addbutton&filename=default.black&newItem="+url);
 	}
 }	
 
 function crawlpage() {
+    var url = window.content.prompt("Index URL:", window._content.location);
+	if(url != null && url != ""){
+		window.open(baseURL + '/QuickCrawlLink_p.html?localIndexing=on&crawlingQ=on&xdstopw=on&title='+escape(document.title)+'&url='+url,'_blank','height=150,width=500,resizable=yes,scrollbar=no,directory=no,menubar=no,location=no');
+	}
 }
 	
 function showAbout() {

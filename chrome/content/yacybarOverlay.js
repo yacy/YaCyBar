cb0804126db08e62a1a314a5779f0b32744f8d20
theli <theli@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2006-01-24 14:37:43 +0000

    *) adding some icons *) adding about menu *) adding direct links to yacy homepage/forum/wiki

--- chrome/content/yacybarOverlay.js
+++ chrome/content/yacybarOverlay.js
@@ -1,12 +1,26 @@
 var gBrowser = document.getElementById("content");
+
 function search(){
 	var searchwords=document.getElementById("search").value;
 	alert(searchwords);
-	loadOneOrMoreURIs("http://brain:18090/index.html?search="+searchwords);
+	loadOneOrMoreURIs("http://localhost:8080/index.html?search="+searchwords);
 }
+
 function blacklistpage(){
 	var url=window.content.prompt("Blacklist URL:", window._content.location);
 	if(url != null && url != ""){
 		gBrowser.addTab("http://localhost:8080/Blacklist_p.html?addbutton=addbutton&filename=default.black&newItem="+url);
 	}
+}	
+
+function crawlpage() {
+}
+	
+function showAbout() {
+	window.openDialog("chrome://yacybar/content/about.xul","switchProxyAbout","centerscreen, chrome, modal");
+}	
+
+function loadURL(newURL) {
+	window._content.document.location = newURL;
+	window.content.focus();
 }

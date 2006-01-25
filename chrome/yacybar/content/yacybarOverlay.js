2eff8c1759fe209f7870cc925a4aa2bf159a2c35
theli <theli@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2006-01-25 09:38:00 +0000

    *) new logos *) prefs page to configure peerAddress + peerPort

--- chrome/yacybar/content/yacybarOverlay.js
+++ chrome/yacybar/content/yacybarOverlay.js
@@ -1,17 +1,26 @@
-var baseURL  = "http://localhost:8080";
 var gBrowser = document.getElementById("content");
+var prefManager = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
+
 var crawlingDepth = 0;
+
+function getBaseURL() {
+	var peerAddress = prefManager.getCharPref("extensions.yacybar.peerAddress");	
+	var peerPort = prefManager.getIntPref("extensions.yacybar.peerPort");	
+	var baseURL  = "http://" + peerAddress + ":" + peerPort;
+	
+	return baseURL;
+}
 
 function search(){
 	var searchwords=document.getElementById("search").value;
 	// alert(searchwords);
-	loadOneOrMoreURIs(baseURL + "/index.html?search="+searchwords);
+	loadOneOrMoreURIs(getBaseURL() + "/index.html?search="+searchwords);
 }
 
 function blacklistpage(){
 	var url=window.content.prompt("Blacklist URL:", window._content.location);
 	if(url != null && url != ""){
-		gBrowser.addTab(baseURL + "/Blacklist_p.html?addbutton=addbutton&filename=default.black&newItem="+url);
+		gBrowser.addTab(getBaseURL() + "/Blacklist_p.html?addbutton=addbutton&filename=default.black&newItem="+url);
 	}
 }	
 
@@ -22,12 +31,16 @@ function changeCrawlingDepth(newCrawlingDepth) {
 function crawlpage() {
     var url = window.content.prompt("Index URL with depth '" + crawlingDepth + "':", window._content.location);
 	if(url != null && url != ""){
-		window.open(baseURL + '/QuickCrawlLink_p.html?localIndexing=on&crawlingQ=on&crawlingDepth=' + crawlingDepth + '&xdstopw=on&title='+escape(document.title)+'&url='+url,'_blank','height=150,width=500,resizable=yes,scrollbar=no,directory=no,menubar=no,location=no');
+		window.open(getBaseURL() + '/QuickCrawlLink_p.html?localIndexing=on&crawlingQ=on&crawlingDepth=' + crawlingDepth + '&xdstopw=on&title='+escape(document.title)+'&url='+url,'_blank','height=150,width=500,resizable=yes,scrollbar=no,directory=no,menubar=no,location=no');
 	}
 }
 	
 function showAbout() {
-	window.openDialog("chrome://yacybar/content/about.xul","switchProxyAbout","centerscreen, chrome, modal");
+	window.openDialog("chrome://yacybar/content/about.xul","yacybarAbout","centerscreen, chrome, modal");
+}	
+
+function showPrefs() {
+	window.openDialog("chrome://yacybar/content/prefs.xul","yacybarPrefs","centerscreen, chrome, modal");
 }	
 
 function loadURL(newURL) {

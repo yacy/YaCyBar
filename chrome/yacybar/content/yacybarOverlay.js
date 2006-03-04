eb29ea49df074bfd42abaa202693e3e1650c8407
orbiter <orbiter@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2006-03-04 18:55:02 +0000

    adopted search page url to svn 1815 / new yacysearch page

--- chrome/yacybar/content/yacybarOverlay.js
+++ chrome/yacybar/content/yacybarOverlay.js
@@ -105,7 +105,7 @@ function getSearchURL(keyword) {
 	if (urlMask == "") urlMask = ".*";
 	
 	return getBaseURL() + 
-	"/index.html" + 
+	"/yacysearch.html" + 
 	"?search=" + keyword + 
 	"&count=" + maxResults + 
 	"&order=" + orderBy + 

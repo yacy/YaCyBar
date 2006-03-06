0a53b45b0cdeabc7a490c81610fd8dce6127054a
allo <allo@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2006-03-06 21:39:03 +0000

    js for the last commit

---
+++ chrome/yacybar/content/firststart.js
@@ -0,0 +1,14 @@
+function toggleDemo(){
+	win=window.arguments[0]
+	alert("On next start of firefox you need to reenable the Demomode. You can find it then in the YaCy-Menu.");
+	window.openDialog("chrome://yacybar/content/demopeers.xul", "demopeerDialog", "centerscreen, chrome, modal", win.document);
+	window.close();
+}
+function downloadYacy(){
+	window.open('http://www.yacy.net/yacy/');
+	window.close();
+}
+function showPref(){
+	window.arguments[0].openPreferences('YaCyBarPrefsPane');
+	window.close();
+}

dda58a58ac8b3281af884c173cad208425426ddd
f1ori <f1ori@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2008-04-03 19:36:34 +0000

    * unified formatting (only tabs for indention and only unix linebreaks) (part 2) * no functional changes

--- chrome/yacybar/content/demopeers.js
+++ chrome/yacybar/content/demopeers.js
@@ -1,30 +1,33 @@
 var peers;
 var index;
-var mainWindow
-function init(){
-		mainWindow=window.arguments[0]
-		var demoPeersURL=Branch.getCharPref("demoPeersURL");
-		req=new XMLHttpRequest();
-		req.open("GET", demoPeersURL, false);
-		req.send(null);
-		response=req.responseXML;
-		peers=response.getElementsByTagName("peer");
-		list=document.getElementById("demopeerList");
-		for(i=0;i<peers.length;i++){
-			item=document.createElement("listitem");
-			item.setAttribute("label", peers[i].getAttribute("name"));
-			item.setAttribute("value", peers[i].getAttribute(i));
-			list.appendChild(item);
-		}
-		list.selectedIndex=Math.floor(Math.random()*peers.length);
-		selectDemopeer();
+var mainWindow;
+
+function init() {
+	mainWindow = window.arguments[0];
+	var demoPeersURL = Branch.getCharPref("demoPeersURL");
+	req = new XMLHttpRequest();
+	req.open("GET", demoPeersURL, false);
+	req.send(null);
+	response = req.responseXML;
+	peers = response.getElementsByTagName("peer");
+	list = document.getElementById("demopeerList");
+	for(i=0; i<peers.length; i++) {
+		item = document.createElement("listitem");
+		item.setAttribute("label", peers[i].getAttribute("name"));
+		item.setAttribute("value", peers[i].getAttribute(i));
+		list.appendChild(item);
+	}
+	list.selectedIndex = Math.floor(Math.random()*peers.length);
+	selectDemopeer();
 }
-function selectDemopeer(){
-	list=document.getElementById("demopeerList");
-	index=list.selectedIndex;
+
+function selectDemopeer() {
+	list = document.getElementById("demopeerList");
+	index = list.selectedIndex;
 }
-function accept(){
-	if(index>=0){
+
+function accept() {
+	if(index >= 0){
 		Branch.setBoolPref("demomode",true);
 		Branch.setCharPref("demoAddress", peers[index].getElementsByTagName("address")[0].getAttribute("host"));
 		Branch.setIntPref("demoPort", parseInt(peers[index].getElementsByTagName("address")[0].getAttribute("port")));
@@ -35,6 +38,6 @@ function accept(){
 	mainWindow.getElementById("cmd_toggleIndexing").setAttribute("disabled", true);
 	mainWindow.getElementById("BlacklistBtn").setAttribute("disabled", true);
 }
-function cancel(){
+function cancel() {
 	mainWindow.getElementById("menuitem-demo").setAttribute("checked", false);
 }

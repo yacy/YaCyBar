38942da6ce9c98efab85c29632609226e2d206d0
apfelmaennchen <apfelmaennchen@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2009-02-01 20:46:35 +0000

    - added the YaCy folder structure to YaCyBookmarksService

--- components/YaCyBookmarkService.js
+++ components/YaCyBookmarkService.js
@@ -48,21 +48,39 @@ YaCyBookmarkService.prototype = {
 	onContainerNodeOpening: function(aContainer, aOptions) {
 		// import here, because resource is not yet available when loading this component
 		Cu.import("resource://yacybar/functions.jsm");
+		Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 
 		if(aContainer.childCount == 0) {
-			try {
+			try {				
+				var cons = Components.classes["@mozilla.org/consoleservice;1"].getService(Components.interfaces.nsIConsoleService);
+				var bmsvc = Components.classes["@mozilla.org/browser/nav-bookmarks-service;1"]
+                      .getService(Components.interfaces.nsINavBookmarksService);
+				
+				var root = aContainer.title;
+				if (root == "YacyBookmarks") {
+					root = "source";
+				}
+				
+				cons.logStringMessage("root: "+root);
 				var req = Cc["@mozilla.org/xmlextras/xmlhttprequest;1"].createInstance();
-				var url = yacyFunctions.getBaseURL() + "/xml/bookmarks/xbel/xbel.xml?login=true";
+				var url = yacyFunctions.getBaseURL() + "/api/bookmarks/get_folders.xml?bmtype=title&root="+root;
 				var userPwd = yacyFunctions.loadUserPwd();
 				req.open('GET', url, false, userPwd["user"], userPwd["pwd"]);
 				req.send(null);
 				var xml = req.responseXML;
-				var bookmarks = xml.getElementsByTagName("bookmark");
+				var bookmarks = xml.getElementsByTagName("folder");
 				for(var i=0; i<bookmarks.length; i++) {
 					var bookmark = bookmarks[i];
-					var url = bookmark.getAttribute("href");
-					var title = bookmark.getElementsByTagName("title")[0].firstChild.nodeValue;
-					aContainer.appendURINode(url, title, 0, 0, null);
+					var url = bookmark.getAttribute("url");
+					var id = bookmark.getAttribute("id");
+					var name = bookmark.getAttribute("name");
+					var type = bookmark.getAttribute("type");
+					if (type == "file") {
+						aContainer.appendURINode(url, name, 0, 0, null);
+					} else if (type == "folder") {							
+						var newFolderID = bmsvc.createDynamicContainer(aContainer.itemid, id, "@yacy.net/YaCyBookmarkService;1", bmsvc.DEFAULT_INDEX);																		
+						aContainer.appendFolderNode(newFolderID);
+					}
 				}
 			} catch(e) {
 				yacyFunctions.log(e);
